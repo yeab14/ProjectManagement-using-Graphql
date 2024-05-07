99aaef198c84bd312a279324a27da3bf0061eece
yeab14 <155532389+yeab14@users.noreply.github.com>
2024-05-07 06:08:43 -0700

    project management using Graphql

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
+  try {
+    const conn = await mongoose.connect("mongodb://localhost:27017/my_database", {
+      
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
+  } catch (err) {
+    console.error(`Error connecting to MongoDB: ${err.message}`.red.underline.bold);
+    process.exit(1); 
+  }
 };
 
 module.exports = connectDB;
